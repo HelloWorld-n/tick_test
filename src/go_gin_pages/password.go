a59693093f31bdf7741a61892648440f7107101e
HelloWorld-n <gajica.b@gmail.com>
2025-03-09 16:57:00 +0100

    allow password/simple to use Size

--- src/go_gin_pages/password.go
+++ src/go_gin_pages/password.go
@@ -6,14 +6,35 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 var passwords []string = make([]string, 0)
 
 type passwordSimpleConfig struct {
-	MinSize int      `json:"MinSize"  binding:"required,gt=0"`
-	MaxSize int      `json:"MaxSize"  binding:"required,gtefield=MinSize"`
+	Size    int      `json:"Size"     binding:"omitempty,gt=0"`
+	MinSize int      `json:"MinSize"  binding:"omitempty,gt=0"`
+	MaxSize int      `json:"MaxSize"  binding:"omitempty,gtefield=MinSize"`
 	Charset []string `json:"Charset"  binding:"required,min=2,dive,required"`
+}
+
+func passwordConfigStructLevelValidation(sl validator.StructLevel) {
+	config := sl.Current().Interface().(passwordSimpleConfig)
+
+	providedSize := config.Size > 0
+	providedMinAndMax := (config.MinSize > 0) && (config.MaxSize > 0)
+	providedMinOrMax := (config.MinSize > 0) || (config.MaxSize > 0)
+	providedMinXorMan := (config.MinSize > 0) != (config.MaxSize > 0)
+
+	if providedSize && providedMinOrMax {
+		sl.ReportError(config.Size, "Size", "Size", "nand", "MinSize,MaxSize")
+	}
+	if !providedSize && !providedMinAndMax {
+		sl.ReportError(config.Size, "Size", "Size", "or", "MinSize,MaxSize")
+	}
+	if providedMinXorMan {
+		sl.ReportError(config.Size, "(MinSize,MaxSize)", "MinSize", "nxor", "MaxSize")
+	}
 }
 
 func findAllPasswords(c *gin.Context) {
@@ -23,6 +44,14 @@ func findAllPasswords(c *gin.Context) {
 	)
 }
 
+func determineSize(data passwordSimpleConfig) int {
+	if data.Size > 0 {
+		return data.Size
+	} else {
+		return data.MinSize + rand.Intn(data.MaxSize-data.MinSize+1)
+	}
+}
+
 func createSimplePassword(c *gin.Context) {
 	var password string
 	var data passwordSimpleConfig
@@ -30,7 +59,11 @@ func createSimplePassword(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	for range data.MinSize + rand.Intn(data.MaxSize-data.MinSize+1) {
+	if err := passwordValidator.Struct(data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	for range determineSize(data) {
 		randomIndex := rand.Intn(len(data.Charset))
 		randomElement := (data.Charset)[randomIndex]
 		password = fmt.Sprint(password, randomElement)
@@ -43,7 +76,11 @@ func createSimplePassword(c *gin.Context) {
 	)
 }
 
+var passwordValidator = validator.New()
+
 func preparePassword(route *gin.RouterGroup) {
+	passwordValidator.RegisterStructValidation(passwordConfigStructLevelValidation, passwordSimpleConfig{})
+
 	route.GET("", findAllPasswords)
 	route.POST("/simple", createSimplePassword)
 }
